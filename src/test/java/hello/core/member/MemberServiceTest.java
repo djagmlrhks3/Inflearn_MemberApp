f768786a1c3596ef42af1108701cc714da97d45e
djagmlrhks3 <djagmlrhks3@naver.com>
2022-03-22 23:16:35 +0900

    220322 | Inflearn | AppConfig

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,23 +1,34 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    @Test
-    void join() {
-        //given
-        Member member = new Member(1L, "memberA", Grade.VIP);
+//        MemberService memberService = new MemberServiceImpl();
+        MemberService memberService;
 
-        //when
-        memberService.join(member);
-        Member findMember = memberService.findMember(1L);
-        //Member findMember = memberService.findMember(2L);
+        @BeforeEach
+        public void beforeEach() {
+            AppConfig appConfig = new AppConfig();
+            memberService = appConfig.memberService();
+        }
 
-        //then
-        Assertions.assertThat(member).isEqualTo(findMember);
+        @Test
+        void join() {
+            //given
+            Member member = new Member(1L, "memberA", Grade.VIP);
 
+            //when
+            memberService.join(member);
+            Member findMember = memberService.findMember(1L);
+            //Member findMember = memberService.findMember(2L);
+
+            //then
+            Assertions.assertThat(member).isEqualTo(findMember);
+
+        }
     }
-}
+
